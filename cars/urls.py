bae61a13bd206e22220fb4d0a7daf0a71f8bebdc
developdjango <pythondjango726@gmail.com>
2021-09-03 23:43:42 +0530

    create urls.py:car_detial,views.py, car_detial.html

--- cars/urls.py
+++ cars/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from cars import views
 urlpatterns = [
     path('car/', views.car, name='car'),
+    path('<int:id>', views.car_detail, name='car_detail'),
 ]
--- cars/views.py
+++ cars/views.py
@@ -5,3 +5,7 @@ from django.shortcuts import render
 
 def car(request):
     return render(request, 'cars/car.html')
+
+
+def car_detail(request, id):
+    return render(request, 'cars/car_detail.html')
